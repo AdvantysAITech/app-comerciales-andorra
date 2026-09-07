5b1df407bcf4c6e0aec1c84b40da01715563b19f
Mustardinho <98399682+Mustardinho@users.noreply.github.com>
2026-09-07 17:00:45 +0200

    Bug fix colores documentos generados

--- app/documentos/layout.tsx
+++ app/documentos/layout.tsx
@@ -3,5 +3,5 @@ export default function LayoutDocumento({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="min-h-screen bg-white">{children}</div>;
+  return <div className="min-h-screen bg-lienzo">{children}</div>;
 }
